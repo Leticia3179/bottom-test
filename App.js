c6fcf18656593ea8b0299b20287b358fd791ed8a
Leticia3179 <leticiasampaio170305@gmail.com>
2025-06-05 21:28:59 -0300

    conseguiii

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
+// Telas podem ficar fora do componente principal
+function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Home</Text>
     </View>
   );
 }
 
+function ContatosScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Contatos</Text>
+    </View>
+  );
+}
+
+function AjudaScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Ajuda</Text>
+    </View>
+  );
+}
+
+function SobreScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>sobre a App</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Contatos" component={ContatosScreen} />
+        <Tab.Screen name="Ajuda" component={AjudaScreen} />
+        <Tab.Screen name="Sobre" component={SobreScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
 });
